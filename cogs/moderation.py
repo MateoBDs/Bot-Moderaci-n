9bf2bb27e8e8eb706e948415ca7c9010e2e99bb2
MateoBD <mateobeldiaz@gmail.com>
2026-07-05 10:51:00 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -4,18 +4,26 @@ from discord import app_commands
 
 GUILD_ID = 1522869805462589593
 
+
 class Moderation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.guilds(discord.Object(id=GUILD_ID))
+    @app_commands.guilds(
+        discord.Object(id=GUILD_ID)
+    )
     @app_commands.command(
         name="ping",
         description="Comprueba si el bot funciona"
     )
     async def ping(self, interaction: discord.Interaction):
-        await interaction.response.send_message("🏓 Pong!")
+        await interaction.response.send_message(
+            "🏓 Pong!"
+        )
+
 
 async def setup(bot):
-    await bot.add_cog(Moderation(bot))
+    await bot.add_cog(
+        Moderation(bot)
+    )
