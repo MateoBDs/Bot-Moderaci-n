34a5f1dfc8206bbd4808f92a23c48c0e5ec994a0
MateoBD <mateobeldiaz@gmail.com>
2026-07-05 12:09:56 +0100

    Update logs.py

--- utils/logs.py
+++ utils/logs.py
@@ -1,1 +1,92 @@
+import aiosqlite
+import discord
 
+DB_NAME = "database.db"
+
+
+async def get_guild_config(guild_id: int):
+
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        cursor = await db.execute(
+            """
+            SELECT
+                logs_channel,
+                punishments_channel,
+                mod_role
+            FROM guild_config
+            WHERE guild_id = ?
+            """,
+            (guild_id,)
+        )
+
+        return await cursor.fetchone()
+
+
+async def send_log(
+    guild: discord.Guild,
+    embed: discord.Embed
+):
+
+    data = await get_guild_config(
+        guild.id
+    )
+
+    if not data:
+        return
+
+    logs_channel = data[0]
+
+    if not logs_channel:
+        return
+
+    channel = guild.get_channel(
+        logs_channel
+    )
+
+    if channel:
+        await channel.send(
+            embed=embed
+        )
+
+
+async def send_punishment(
+    guild: discord.Guild,
+    content: str,
+    embed: discord.Embed
+):
+
+    data = await get_guild_config(
+        guild.id
+    )
+
+    if not data:
+        return
+
+    punishments_channel = data[1]
+
+    if not punishments_channel:
+        return
+
+    channel = guild.get_channel(
+        punishments_channel
+    )
+
+    if channel:
+        await channel.send(
+            content=content,
+            embed=embed
+        )
+
+
+async def send_dm(
+    member: discord.Member,
+    embed: discord.Embed
+):
+
+    try:
+        await member.send(
+            embed=embed
+        )
+    except:
+        pass
