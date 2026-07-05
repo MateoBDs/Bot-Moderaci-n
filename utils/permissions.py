617f17c084e523ec7dbf80e927e0dcda3b81f1dd
MateoBD <mateobeldiaz@gmail.com>
2026-07-05 14:15:01 +0100

    Update permissions.py

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,29 +1,19 @@
 import aiosqlite
 
-DB_NAME = "database.db"
+DB_NAME = "data/database.db"
 
 
 async def is_mod(member):
-
     async with aiosqlite.connect(DB_NAME) as db:
-
-        cursor = await db.execute(
-            """
-            SELECT mod_role
-            FROM guild_config
-            WHERE guild_id = ?
-            """,
-            (member.guild.id,)
-        )
+        cursor = await db.execute("""
+        SELECT mod_role
+        FROM guild_config
+        WHERE guild_id = ?
+        """, (member.guild.id,))
 
         data = await cursor.fetchone()
 
-    if not data:
+    if not data or not data[0]:
         return False
 
-    mod_role = data[0]
-
-    if not mod_role:
-        return False
-
-    return any(role.id == mod_role for role in member.roles)
+    return any(r.id == data[0] for r in member.roles)
